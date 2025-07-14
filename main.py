a578ec11b1f58f7e368816c65217d2a09f6b53de
Shallot-2009 <3405802009@qq.com>
2025-07-14 21:51:37 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+import os
+
+#os.system('python .\simulation\Ansys_HFSS\models\Wifi_patch_antenna.py')
+#os.system('python .\simulation\Ansys_HFSS\models\Inset_Fed_Rectangular_Patch_Antenna.py')
+#os.system('python .\simulation\Ansys_HFSS\models\Edge_Fed_Rectangular_Patch_Antenna.py')
+#os.system('python .\simulation\Ansys_HFSS\models\Planar_InvertedF_Antenna.py')
+os.system('python .\simulation\Ansys_HFSS\models\Inset_Fed_Elliptical_Patch_Antenna.py')
