2bd1d9a7227fb850150d1368922156554be34ba9
Shallot-2009 <3405802009@qq.com>
2025-08-14 02:07:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # main.py
 import os
 
-#os.system('python .\simulation\Ansys_HFSS\models\Wifi_patch_antenna.py')
-#os.system('python .\simulation\Ansys_HFSS\models\Inset_Fed_Rectangular_Patch_Antenna.py')
-#os.system('python .\simulation\Ansys_HFSS\models\Edge_Fed_Rectangular_Patch_Antenna.py')
-#os.system('python .\simulation\Ansys_HFSS\models\Planar_InvertedF_Antenna.py')
-os.system('python .\simulation\Ansys_HFSS\models\Inset_Fed_Elliptical_Patch_Antenna.py')
+#os.system('python .\\simulation\\Ansys_HFSS\\models\\Wifi_patch_antenna.py')
+#os.system('python .\\simulation\\Ansys_HFSS\\models\\Inset_Fed_Rectangular_Patch_Antenna.py')
+#os.system('python .\\simulation\\Ansys_HFSS\\models\\Edge_Fed_Rectangular_Patch_Antenna.py')
+#os.system('python .\\simulation\\Ansys_HFSS\\models\\Planar_InvertedF_Antenna.py')
+os.system('python .\\simulation\\Ansys_HFSS\\models\\Inset_Fed_Elliptical_Patch_Antenna.py')
+
+
+#os.system('python .\\simulation\\Ansys_HFSS_from_si9000\\models\\Surface_Microstrip_1B.py')
+#os.system('python .\\simulation\\Ansys_HFSS_from_si9000\\models\\Edge-Coupled_Offset_Stripline_1B1A.py')
